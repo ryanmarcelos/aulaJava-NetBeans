482fb86cb799c383aebbfc867b139c010fb642a9
RYAN <126591328+ryanmarcelos@users.noreply.github.com>
2025-08-29 09:04:52 -0300

    Add files via upload

---
+++ Aula2/src/main/java/com/mycompany/aula2/Aula2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aula2;
+
+/**
+ *
+ * @author aluno
+ */
+public class Aula2 {
+
+    public static void main(String[] args) {
+        
+        Pessoa eu = new Pessoa();
+        //eu.nome = "Henrique";
+        
+        System.out.println(eu.nome);
+        System.out.println("Idade: " + eu.idade);
+        System.out.println("Sexo: " + eu.sexo);
+        System.out.println("Estado Civil: " + eu.estadoCivil);
+        System.out.println("Hobby: " + eu.hobby);
+    }
+}
